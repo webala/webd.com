8190b52e24c8ddedb9acd45c10adf0dc2bffa139
webala <webdspam@gmail.com>
2023-01-04 17:24:06 +0300

    add: projects

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,45 +1,43 @@
-import React from 'react'
-import "./Projects.scss"
+/** @format */
+
+import React from "react";
+import "./Projects.scss";
 import { FiExternalLink, FiGithub } from "react-icons/fi";
-import project_img from '../../assets/projects/project1.png'
 
-function Project() {
-  return (
-     <div className="project">
-        <div className="image">
-           <div className="container">
-              <img src={project_img} alt="project" />
-           </div>
-        </div>
-        <div className="details">
-           <h1>Wubalubadubdub</h1>
-           <p className="project-type">Personal project</p>
+function Project({ project }) {
+   return (
+      <div className="project">
+         <div className="image">
+            <div className="container">
+               <img src={project.image} alt="project" />
+            </div>
+         </div>
+         <div className="details">
+            <h1>{project.name}</h1>
+            <p className="project-type">{project.projectType}</p>
 
-           <div className="description">
-              <p>
-                 Wubalubadubdub is an app that you can use to browse characters,
-                 epispdes and different locations that exist in the famous adult
-                 TV show Rick And Morty. The weird project name is a peculiar
-                 catch phrase that Rick Sanchez, the main character occasionally
-                 utters in the show.
-              </p>
-           </div>
-           <div className="tools">
-              <p>React</p>
-              <p>TypeScript</p>
-              <p>Sass</p>
-           </div>
-           <div className="links">
-              <a href="#">
-                 <FiExternalLink />
-              </a>
-              <a href="#">
-                 <FiGithub />
-              </a>
-           </div>
-        </div>
-     </div>
-  );
+            <div className="description">
+               <p>{project.projectDescription}</p>
+            </div>
+            <div className="tools">
+               {project.tools.map((tool, index) => (
+                  <p key={index}>{tool}</p>
+               ))}
+            </div>
+            <div className="links">
+               {project.links.url && (
+                  <a href={project.links.url} target="_blank">
+                     <FiExternalLink />
+                  </a>
+               )}
+               
+               <a href={project.links.github} target="_blank">
+                  <FiGithub />
+               </a>
+            </div>
+         </div>
+      </div>
+   );
 }
 
-export default Project
+export default Project;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,19 +1,68 @@
-import React from 'react'
-import Project from './Project'
+/** @format */
+
+import React from "react";
+import Project from "./Project";
+import project1 from "../../assets/projects/project1.png";
+import project2 from "../../assets/projects/project2.png";
+import project3 from "../../assets/projects/project3.png";
+
+
+
+const projects = [
+   {
+      name: "Wubalubadubdub",
+      projectType: "Personal project",
+      projectDescription:
+         "Wubalubadubdub is an app that you can use to browse characters, epispdes and different locations that exist in the famous adult TV show Rick And Morty. The weird project name is a peculiar catch phrase that Rick Sanchez, the main character occasionally utters in the show.",
+      tools: ["React", "TypeScript", "Sass"],
+      links: {
+         github: "https://github.com/webala/wubalubadubdub",
+         url: "https://wubalubadubdub-mu.vercel.app/",
+      },
+      image: project1,
+   },
+   {
+      name: "Veannes",
+      projectType: "Freelance Project",
+      projectDescription:
+         "This is a fully functional ecommerce website complete with payment intergration using M-pesa Daraja API. It is provides a solution for people with small businesses operating in a paticular geographical location.",
+      tools: ["Django", "Tailwind", "Html and Css", "JavaScript"],
+      links: {
+         github: "https://github.com/webala/ecom-classic",
+      },
+      image: project2,
+   },
+   {
+      name: "Glitch Cloud Photography",
+      projectType: "Freelance project",
+      projectDescription:
+         "A website designed to showcase and sell services by a photographer. It harneses the power of animations to capture the attention of users. This website is still a work in progress. The complete website provides clients the ability to book photoshoot sessions and make down payments seemlessly using M-Pesa Daraja API.",
+      tools: ["React", "TypeScript", "Sass", "NextJS"],
+      links: {
+         github: "https://github.com/webala/glitch-cloud",
+         url: "https://glitch-cloud.vercel.app/",
+      },
+      image: project3,
+   },
+];
 
 function Projects() {
-  return (
-     <div className="projects">
-        <div className="title">
-           <div>
-              <span>02.</span>
-              <h1>Some things i've built</h1>
-           </div>
-           <div className="line"></div>
-        </div>
-        <Project />
-     </div>
-  );
+   return (
+      <div className="projects">
+         <div className="title">
+            <div>
+               <span>02.</span>
+               <h1>Some things i've built</h1>
+            </div>
+            <div className="line"></div>
+         </div>
+
+         {projects.map((project, index) => (
+            <Project key={index} project={project} />
+         ))}
+
+      </div>
+   );
 }
 
-export default Projects
+export default Projects;
