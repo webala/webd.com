829e43a60105480074f546d92c741a9a47d1c987
webala <webd01@protonmail.com>
2023-01-10 14:14:59 +0300

    add: project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,76 +6,85 @@ import project1 from "../../assets/projects/project1.png";
 import project2 from "../../assets/projects/project2.png";
 import project3 from "../../assets/projects/project3.png";
 import project4 from "../../assets/projects/project4.png";
-
-
+import project5 from "../../assets/projects/project5.png";
 
 const projects = [
-   {
-      name: "Wubalubadubdub",
-      projectType: "Personal project",
-      projectDescription:
-         "Wubalubadubdub is an app that you can use to browse characters, epispdes and different locations that exist in the famous adult TV show Rick And Morty. The weird project name is a peculiar catch phrase that Rick Sanchez, the main character occasionally utters in the show.",
-      tools: ["React", "TypeScript", "Sass"],
-      links: {
-         github: "https://github.com/webala/wubalubadubdub",
-         url: "https://wubalubadubdub-mu.vercel.app/",
-      },
-      image: project1,
-   },
-   {
-      name: "Veannes",
-      projectType: "Freelance Project",
-      projectDescription:
-         "This is a fully functional ecommerce website complete with payment intergration using M-pesa Daraja API. It is provides a solution for people with small businesses operating in a paticular geographical location.",
-      tools: ["Django", "Tailwind", "Html and Css", "JavaScript"],
-      links: {
-         github: "https://github.com/webala/ecom-classic",
-      },
-      image: project2,
-   },
-   {
-      name: "Glitch Cloud Photography",
-      projectType: "Freelance project",
-      projectDescription:
-         "A website designed to showcase and sell services by a photographer. It harneses the power of animations to capture the attention of users. This website is still a work in progress. The complete website provides clients the ability to book photoshoot sessions and make down payments seemlessly using M-Pesa Daraja API.",
-      tools: ["React", "TypeScript", "Sass", "NextJS"],
-      links: {
-         github: "https://github.com/webala/glitch-cloud",
-         url: "https://glitch-cloud.vercel.app/",
-      },
-      image: project3,
-   },
-   {
-      name: "Space Cadet",
-      projectType: "Frontend mentor",
-      projectDescription:
-         "This project shows information about space. It is a front end mentor challenge. The major take away from this project was the use of animations using `react-animation-on-scroll`. I aslo practiced my TypeScript.",
-      tools: ["React", "TypeScript", "Sass", "NextJS"],
-      links: {
-         github: "https://github.com/webala/space-tourism-site",
-         url: "https://space-tourism-site-beta.vercel.app/",
-      },
-      image: project4,
-   },
+	{
+		name: "Wubalubadubdub",
+		projectType: "Personal project",
+		projectDescription:
+			"Wubalubadubdub is an app that you can use to browse characters, epispdes and different locations that exist in the famous adult TV show Rick And Morty. The weird project name is a peculiar catch phrase that Rick Sanchez, the main character occasionally utters in the show.",
+		tools: ["React", "TypeScript", "Sass"],
+		links: {
+			github: "https://github.com/webala/CakeFiary",
+			url: "https://wubalubadubdub-mu.vercel.app/",
+		},
+		image: project1,
+	},
+	{
+		name: "Cake Fairy",
+		projectType: "Freelance project",
+		projectDescription: "Cake Fairy is a website used by a cake business. Other than its awesome interface, users can place orders for cakes and make payments online in a secure way.",
+		tools: ["React", "TypeScript", "Sass", "NextJS"],
+		links: {
+			github: "https://github.com/webala/wubalubadubdub",
+			url: "https://cake-fiary.vercel.app/",
+		},
+		image: project5,
+	},
+	{
+		name: "Veannes",
+		projectType: "Freelance Project",
+		projectDescription:
+			"This is a fully functional ecommerce website complete with payment intergration using M-pesa Daraja API. It is provides a solution for people with small businesses operating in a paticular geographical location.",
+		tools: ["Django", "Tailwind", "Html Css", "JavaScript"],
+		links: {
+			github: "https://github.com/webala/ecom-classic",
+		},
+		image: project2,
+	},
+	{
+		name: "Glitch Cloud Photography",
+		projectType: "Freelance project",
+		projectDescription:
+			"A website designed to showcase and sell services by a photographer. It harneses the power of animations to capture the attention of users. This website is still a work in progress. The complete website provides clients the ability to book photoshoot sessions and make down payments seemlessly using M-Pesa Daraja API.",
+		tools: ["React", "TypeScript", "Sass", "NextJS"],
+		links: {
+			github: "https://github.com/webala/glitch-cloud",
+			url: "https://glitch-cloud.vercel.app/",
+		},
+		image: project3,
+	},
+	{
+		name: "Space Cadet",
+		projectType: "Frontend mentor",
+		projectDescription:
+			"This project shows information about space. It is a front end mentor challenge. The major take away from this project was the use of animations using `react-animation-on-scroll`. I aslo practiced my TypeScript.",
+		tools: ["React", "TypeScript", "Sass", "NextJS"],
+		links: {
+			github: "https://github.com/webala/space-tourism-site",
+			url: "https://space-tourism-site-beta.vercel.app/",
+		},
+		image: project4,
+	},
 ];
 
 function Projects() {
-   return (
-      <div className="projects">
-         <div className="title">
-            <div>
-               <span>02.</span>
-               <h1>Some things i've built</h1>
-            </div>
-            <div className="line"></div>
-         </div>
+	return (
+		<div className="projects">
+			<div className="title">
+				<div>
+					<span>02.</span>
+					<h1>Some things i've built</h1>
+				</div>
+				<div className="line"></div>
+			</div>
 
-         {projects.map((project, index) => (
-            <Project key={index} project={project} />
-         ))}
-
-      </div>
-   );
+			{projects.map((project, index) => (
+				<Project key={index} project={project} />
+			))}
+		</div>
+	);
 }
 
 export default Projects;
